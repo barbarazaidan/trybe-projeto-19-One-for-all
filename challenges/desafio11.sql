98c5d0ba484c1a2c38d613e3de1b0c7dd5fd873c
Bárbara <babivaldez@gmail.com>
2023-05-22 18:39:35 -0300

    req11 - Crie uma QUERY que exibe o top 3 de álbuns com as músicas que mais foram favoritadas

---
+++ challenges/desafio11.sql
@@ -0,0 +1,11 @@
+SELECT 
+    COUNT(favoritas.musica_id) AS favoritadas,
+    Alb.album AS album
+FROM
+    SpotifyClone.Musicas_favoritas AS favoritas
+        INNER JOIN
+    SpotifyClone.Musicas AS music ON music.musica_id = favoritas.musica_id
+        INNER JOIN
+    SpotifyClone.Albuns AS Alb ON music.album_id = Alb.album_id
+GROUP BY Alb.album
+ORDER BY favoritadas DESC LIMIT 3;
